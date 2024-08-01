5c15c2fd06c09c2439ca6747c67ad06e48641fe2
Dennis Sejas <dedasema734012@gmail.com>
2024-07-31 21:14:09 -0400

    chore: Update HomeController to fetch data from the contacts table

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\Contact;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return $this->view('home');
+        $contactModel = new Contact();
+        $contactModel->query("SELECT * FROM contacts")->get();
+        
+        return $this->view('home',[
+            'title' => 'Bienvenido',
+            'description' => 'Este es un sistema de prueba'
+        ]);
     }
 }
