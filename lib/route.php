672bb6ff1826326c3b43d9befa944b73004e1f54
Dennis Sejas <dedasema734012@gmail.com>
2024-08-03 00:36:39 -0400

    chore: Update route.php to handle query parameters in URI

--- lib/route.php
+++ lib/route.php
@@ -24,12 +24,16 @@ class Route{
         $uri = $_SERVER['REQUEST_URI'];
         $uri = trim($uri, '/');
 
+        if(strpos($uri, '?')){
+            $uri = substr($uri, 0, strpos($uri, '?'));
+        }
+
         $method = $_SERVER['REQUEST_METHOD'];
 
         foreach(self::$routes[$method] as $route => $callback){
 
             if(strpos($route, ':') !== false){
-                $route = preg_replace("#:[a-zA-Z]+#", "([a-zA-Z]+)", $route);
+                $route = preg_replace("#:[a-zA-Z]+#", "([a-zA-Z0-9]+)", $route);
             }
 
             if(preg_match("#^$route$#", $uri, $matches)){
