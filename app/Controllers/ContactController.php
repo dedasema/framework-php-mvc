7c255a4aefdbaab5334d8e91bb8aecf17cc74e23
Dennis Sejas <dedasema734012@gmail.com>
2024-08-03 00:36:14 -0400

    feat: Add ContactController for managing contacts

---
+++ app/Controllers/ContactController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Contact;
+
+class ContactController extends Controller
+{
+    public function index()
+    {
+        $model = new Contact;
+
+        return $model->select('id','name','phone')
+                     ->where('id', '>', 1)
+                     ->orderBy('id','DESC')
+                     //->paginate(1);
+                     ->get();
+
+        if(isset($_GET['search'])){
+            $contacts = $model->where('name', 'LIKE' , "%{$_GET['search']}%")->paginate(1);
+        }else{
+            $contacts = $model->paginate(1);
+        }
+
+        return $this->view('contacts.index', compact('contacts'));
+    }
+
+    public function create()
+    {
+        return $this->view('contacts.create');
+    }
+
+    public function store()
+    {
+        $data = $_POST;
+        $model = new Contact;
+        $model->create($data);
+        return $this->redirect('/contacts');
+    }
+
+    public function show($id)
+    {
+        $model = new Contact;
+        $contact = $model->find($id);
+        return $this->view('contacts.show', compact('contact'));
+    }
+
+    public function edit($id)
+    {
+        $model = new Contact;
+        $contact = $model->find($id);
+        return $this->view('contacts.edit', compact('contact'));
+    }
+
+    public function update($id)
+    {
+        $data = $_POST;
+        $model = new Contact;
+        $model->update($id, $data);
+        return $this->redirect("/contacts/{$id}");
+    }
+
+    public function destroy($id)
+    {
+        $model = new Contact;
+        $model->delete($id);
+        return $this->redirect('/contacts');
+    }
+}
